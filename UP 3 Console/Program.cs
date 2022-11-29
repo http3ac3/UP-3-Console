73f75eb4980bc40759b8eb15f93f39e05e966322
http3ac3 <ura19092004@gmail.com>
2022-11-29 20:03:41 +0300

    Добавьте файлы проекта.

---
+++ UP 3 Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_3._1_Console
+{
+    internal class Program
+    {
+        static double Function(double x) => x * x * x - Math.Sin(x);
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            Console.Write("Введите а: ");
+            while (!double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Были введены неверные данные! Попробуйте снова!");
+                Console.Write("Введите а: ");
+            }
+
+            Console.Write("Введите b: ");
+            while (!double.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Были введены неверные данные! Попробуйте снова!");
+                Console.Write("Введите b: ");
+            }
+
+            double f_a = Function(a);
+            double f_b = Function(b);
+
+            Console.WriteLine($"В точке a ({a}) функция принимает значение {f_a}");
+            Console.WriteLine($"В точке b ({b}) функция принимает значение {f_b}");
+
+            string answ = (f_a > f_b) ? "Наибольшее значение функция принимает в точке а" : (f_a < f_b) ?
+                "Наибольшее значение функция принимает в точке b" : "Значения функции в двух точках равны";
+            Console.WriteLine(answ);
+        }
+    }
+}
---
+++ UP 3.2 Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_3._2_Console
+{
+    internal class Program
+    {
+        static double Function(double x, double a) => (x < a) ? 0 : (x > a) ? (x - a) / (x + a) : 1; 
+
+        static void Main(string[] args)
+        {
+            double a, b, h;
+            Console.Write("Введите а: ");
+            while (!double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Были введены неверные данные! Попробуйте снова!");
+                Console.Write("Введите а: ");
+            }
+
+            Console.Write("Введите b: ");
+            while (!double.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Были введены неверные данные! Попробуйте снова!");
+                Console.Write("Введите b: ");
+            }
+
+            Console.Write("Введите h: ");
+            while (!double.TryParse(Console.ReadLine(), out h))
+            {
+                Console.WriteLine("Были введены неверные данные! Попробуйте снова!");
+                Console.Write("Введите h: ");
+            }
+
+            Console.WriteLine("Таблица значений");
+            Console.WriteLine("x\t|\ty"); Console.WriteLine("__________________");
+            for (double x = a; x <= b; x += h)
+            {
+                Console.WriteLine($"{x}\t|\t{Function(x, a)}");
+            }
+        }
+    }
+}
